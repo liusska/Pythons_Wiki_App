2c9769671bcd7a094a44ac23c4f3a5f7f3844feb
unknown <lyudmila_ds@abv.bg>
2021-12-29 21:11:16 +0200

    Added Authentication demo

--- templates_advanced/pythons_app/views.py
+++ templates_advanced/pythons_app/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, redirect
 from .forms import PythonCreateForm
 from .models import Python
+from templates_advanced.core.decorators import any_group_required
+from django.contrib.auth import authenticate, login, logout
 
 
-# Create your views here.
+def sign_in(request):
+    user = authenticate(username='liusska', password='123456')
+    login(request, user)
+    return redirect('index')
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('index')
+
+
 def index(request):
     pythons = Python.objects.all()
     return render(request, 'index.html', {'pythons': pythons})
 
 
+# @login_required(login_url=reverse_lazy('sign in'))
+@any_group_required(groups=['User'])
 def create(request):
     if request.method == 'GET':
         form = PythonCreateForm()
