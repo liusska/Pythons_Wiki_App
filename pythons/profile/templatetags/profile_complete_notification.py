e5696fa912e30964b41eff2411eade756746e8ba
unknown <lyudmila_ds@abv.bg>
2022-01-04 12:03:48 +0200

    Added notification tag

---
+++ pythons/profile/templatetags/profile_complete_notification.py
@@ -0,0 +1,13 @@
+from django.template import Library
+from pythons.profile.models import Profile
+
+register = Library()
+
+
+@register.inclusion_tag('tags/profile_complete_notification.html', takes_context=True)
+def profile_complete_notification(context):
+    user_id = context.request.user.id
+    profile = Profile.objects.get(pk=user_id)
+    return {
+        'is_complete': profile.is_complete,
+    }
--- pythons/profile/views.py
+++ pythons/profile/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from pythons.profile.models import Profile
 from pythons.profile.forms import ProfileForm
