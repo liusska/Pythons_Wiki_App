2696468df2ee46bdddb43b8506c820959578065a
unknown <lyudmila_ds@abv.bg>
2022-01-09 00:51:14 +0200

    Change create FBV with CBV, implement mixin for user group permissions

--- pythons/pythons_app/urls.py
+++ pythons/pythons_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name="index"),
+    # path('create/', views.create, name="create"),
+
     path('', views.IndexView.as_view(), name="index"),
-    path('create/', views.create, name="create"),
+    path('create/', views.PythonCreateView.as_view(), name="create"),
 ]
--- pythons/pythons_app/views.py
+++ pythons/pythons_app/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect
 from pythons.core.decorators import any_group_required
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from .forms import PythonCreateForm
 from .models import Python
-
-
-# def index(request):
-#     pythons = Python.objects.all()
-#     return render(request, 'index.html', {'pythons': pythons})
+from pythons.core.mixins import AnyGroupRequiredMixin
 
 
 class IndexView(ListView):
@@ -18,18 +15,30 @@ class IndexView(ListView):
     context_object_name = 'pythons'
 
 
+class PythonCreateView(AnyGroupRequiredMixin, CreateView):
+    template_name = 'create.html'
+    model = Python
+    fields = '__all__'
+    success_url = reverse_lazy('index')
+
+
+# def index(request):
+#     pythons = Python.objects.all()
+#     return render(request, 'index.html', {'pythons': pythons})
+
+
 # @login_required(login_url=reverse_lazy('sign in'))
-@any_group_required(groups=['User'])
-def create(request):
-    if request.method == 'GET':
-        form = PythonCreateForm()
-        return render(request, 'create.html', {'form': form})
-    else:
-        form = PythonCreateForm(request.POST, request.FILES)
-        print(form)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-
-        return render(request, 'create.html', {'form': form})
-
+# @any_group_required(groups=['User'])
+# def create(request):
+#     if request.method == 'GET':
+#         form = PythonCreateForm()
+#         return render(request, 'create.html', {'form': form})
+#     else:
+#         form = PythonCreateForm(request.POST, request.FILES)
+#         print(form)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('index')
+#
+#         return render(request, 'create.html', {'form': form})
+#
