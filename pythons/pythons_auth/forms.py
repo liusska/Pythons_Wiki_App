1ebe444578ad8563d4ec05003e808dac9de7d8fb
unknown <lyudmila_ds@abv.bg>
2022-01-09 00:21:52 +0200

    Update Login and Register FBV with CBV

--- pythons/pythons_auth/forms.py
+++ pythons/pythons_auth/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import authenticate, get_user_model
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 UserModel = get_user_model()
 
@@ -12,17 +13,13 @@ class SignUpForm(UserCreationForm):
         fields = ("email",)
 
 
-class SignInForm(forms.Form):
+class SignInForm(AuthenticationForm):
     user = None
 
-    email = forms.EmailField()
-    password = forms.CharField(
-        widget=forms.PasswordInput(),
-    )
-
-    def clean(self):
+    def clean_password(self):
+        super().clean()
         self.user = authenticate(
-            email=self.cleaned_data['email'],
+            email=self.cleaned_data['username'],
             password=self.cleaned_data['password'],
         )
         if not self.user:
